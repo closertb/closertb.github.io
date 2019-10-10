260e1fa19dd283e1516e6518db223b8c9f78edf5
tangbo <bo.tang3@56qq.com>
2019-10-10 11:28:25 +0800

    feat: delete unused file

--- src/configs/utils.js
+++ src/configs/utils.js
@@ -14,12 +14,3 @@ export function unCompileParam(code = '') {
   }
   return c;
 }
-
-// 加密
-export function compileParam(code = '') {
-  let c = String.fromCharCode(code.charCodeAt(0) + FixOffeset + code.length);
-  for (let i = 1; i < code.length; i += 1) {
-    c += String.fromCharCode(code.charCodeAt(i) + code.charCodeAt(i - 1));
-  }
-  return c; // 增加特殊字符编码，防止'/', '&', '='等字符造成的影响
-}
