2d630cf1301aaee1ab470c593e2cddf4bdd2df16
tangbo <bo.tang3@56qq.com>
2019-10-16 18:11:08 +0800

    feat: update owner with quote

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -3,7 +3,7 @@ export const GENERATE_ARRAY = [1, 2, 3];
 export const SITE_NAME = '吃饭不洗碗'; // 网站标题
 export const SITE_ADDRESS = 'closer.site'; // 网站地址，可为空
 export const SITE_MOTTO = '一个造战斗机转行的前端工程师'; // 一句段子
-export const OWNER = 'closertb'; // 你github name
+export const OWNER = '"closertb"'; // 你github name
 export const PROJECT = '"closertb.github.io"'; // 你issue项目名
 // because of github protect privacy, the token compile with a funciton to encrypted；
 // 由于github安全保护规则，token不能暴露到仓库中，但又因为我们申请的token只是一个只读token，所以这里使用了简单函数进行了对称加解密绕开规则；
