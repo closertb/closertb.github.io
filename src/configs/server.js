d330cf90662e113403cd5a9642d0e4ff359a18e6
closertb <closertb@sina.com>
2020-06-08 22:47:43 +0800

    feat: fix code show height

--- src/configs/server.js
+++ src/configs/server.js
@@ -1,5 +1,5 @@
 export default (env => ({
-  local: 'http://127.0.0.1:3000/arcticle/graphql',
+  local: 'https://closertb.site/arcticle/graphql',
   dev: 'https://closertb.site/arcticle/graphql',
   qa: 'https://closertb.site/arcticle/graphql',
   prod: 'https://closertb.site/arcticle/graphql'
