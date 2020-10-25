49da6f3f4d5a0c436de05910108505bb96264a51
closertb <closertb@sina.com>
2020-10-25 11:20:59 +0800

    feat: 修改项目跳转路径

--- src/configs/menu.js
+++ src/configs/menu.js
@@ -17,17 +17,17 @@ export const Routes = Menu.reduce((pre, { children = [], ...others }) => pre.con
 // 设置项目前，确保相关静态资源已拷贝到public文件夹下，并且文件名大小写与路径一致
 export const NavMenu = [{
   name: '公众号编辑器',
-  path: '/editor',
+  path: '/editor/index.html',
   imgUrl: 'https://doddle.oss-cn-beijing.aliyuncs.com/oldNotes/20200607153230.png',
   git: 'https://github.com/closertb/closertb.github.io.git'
 }, {
   name: '数字华容道',
-  path: '/Klotski',
+  path: '/Klotski/index.html',
   imgUrl: 'https://doddle.oss-cn-beijing.aliyuncs.com/article/klotski.png',
   git: 'https://github.com/closertb/klotski.git'
 }, {
   name: 'Vue可视化图表',
-  path: '/chart',
+  path: '/chart/index.html',
   imgUrl: 'https://doddle.oss-cn-beijing.aliyuncs.com/article/chart.jpg',
   git: 'https://github.com/closertb/simpleEchartsDemo'
 }, {
